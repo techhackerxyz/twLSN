3b8267aa284bcad9e90cf33982c595f4a754be5c
techhackerxyz <techhackerxyz@gmail.com>
2025-07-11 01:55:49 +0530

    Update twLSN.js

--- twLSN.js
+++ twLSN.js
@@ -1,14 +1,8 @@
 var twLSN = [
-  'tokowhatsapp.blogspot.com',
-  'toko-whatsapp.blogspot.com',
-  'jasa-design.web.id',
-  'www.jasa-design.web.id',
-  'storetechhackerxyz.blogspot.com' // your domain
+'tokowhatsapp.blogspot.com',
+'toko-whatsapp.blogspot.com',
+'jasa-design.web.id',
+'www.jasa-design.web.id',
 ];
 
-var redirectUrl = 'http://bit.ly/toko-whatsapp';
-
-var currentHost = window.location.hostname;
-if (!twLSN.includes(currentHost)) {
-  window.location.href = redirectUrl;
-}
+var redirectUrl = 'https://storetechhackerxyz.blogspot.com/';
